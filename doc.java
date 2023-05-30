f52227ea86cbfff18426d6ff2824bef8951f97c7
Evellyn de Oliveira <108280573+evypersonal@users.noreply.github.com>
2023-05-30 13:46:14 -0300

    iniciando java

---
+++ doc.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+//Inicio
+class CalculadoraJava
+{
+  public static void main (String entrada[])
+  {
+    double n1,n2;
+    double op(char);
+    double result;
+
+    case '+':
+    if (n1+n2)
+    {
+      
+    }
+  }
+
+}
